ff89372eb8106997d1afbac9dbd51562180260a4
christensson <marcus.christensson@gmail.com>
2018-01-17 11:36:43 +0100

    Simplify placement by only moving first window on workspace
    This solution relies on the fact that the current mutter placement
    algorithm tries to place subsequent windows next to already existing
    windows. So by moving the first window we will offset the other windows
    to that first window.

--- extension.js
+++ extension.js
@@ -6,32 +6,14 @@ let _workspaceAddedSignal;
 
 let _signals = [];
 
-let verbose = true;
-
-function infoLog() {
-    let args = [].slice.call(arguments);
-    args.unshift("[better] INFO");
-    global.log.apply(global, args);
-}
+let verbose = false;
 
 function debugLog() {
     if (verbose) {
         let args = [].slice.call(arguments);
-        args.unshift("[better] DEBUG");
+        args.unshift("[better-window-placement] DEBUG");
         global.log.apply(global, args);
     }
-}
-
-function matrix(numrows, numcols, getInitial) {
-    var arr = [];
-    for (var i = 0; i < numrows; ++i) {
-        var columns = [];
-        for (var j = 0; j < numcols; ++j) {
-            columns[j] = getInitial();
-        }
-        arr[i] = columns;
-    }
-    return arr;
 }
 
 function _onWindowAdded(workspace, win) {
@@ -45,134 +27,40 @@ function _onWindowAdded(workspace, win) {
     }
 }
 
+function _isWindowHandled(win) {
+  /* 0 = Normal Window, 3 = (non-modal) dialog */
+  return win.get_window_type() == 0 || win.get_window_type() == 3;
+}
+
 function _placeWindow(win, ws) {
-    if (win == null) return;
-    /* 0 = Normal Window, 3 = (non-modal) dialog */
-    if (win.get_window_type() == 0 || win.get_window_type() == 3) {
-        let rect = win.get_frame_rect();//Used in 3.12.2
+    if (win == null) {
+      return;
+    }
 
-        let monitor = Main.layoutManager.monitors[global.screen.get_current_monitor()];
+    if (_isWindowHandled(win)) {
+        // Check if any existing non-minimized windows
+        let numExistingWindows = ws.list_windows().filter(function(item) {
+          return item !== win && _isWindowHandled(item) && !item.minimized;
+        }).length;
+
+        if (numExistingWindows > 0) {
+          debugLog(
+            "Window \"" + win.get_title() + "\": not placed, " +
+            numExistingWindows + " other windows exist"
+          );
+          return;
+        }
+
         let workArea = win.get_work_area_current_monitor();
-        let minX = workArea.x;
-        let minY = workArea.y;
-        let x = monitor.x;
-        let y = monitor.y;
-        let w = rect.width;
-        let h = rect.height;
-        let max_w = workArea.width;
-        let max_h = workArea.height;
+        let x = workArea.x;
+        let y = workArea.y;
 
-        // Divide screen in squares with size equal to window to be added
-        let rows = Math.floor(max_h / h);
-        let cols = Math.floor(max_w / w);
-        let squares = matrix(rows, cols, () => []);
-
-        debugLog("Find placement for (" + win.get_title() + ") - x,y w,h=" + rect.x + "," + rect.y + " " + rect.width + "," + rect.height);
-        debugLog("rows,cols=" + rows + "," + cols);
-
-        // Populate with old windows
-        let windows = ws.list_windows();
-        for (let i = 0; i < windows.length; i++) {
-            let winType = windows[i].get_window_type();
-            if (windows[i] !== win && (winType === 0 || winType === 3) && !windows[i].minimized) {
-                let rect = windows[i].get_frame_rect();
-                let row0 = Math.floor(rect.y / h);
-                let col0 = Math.floor(rect.x / w);
-                let rowCnt = Math.ceil(rect.height / h);
-                let colCnt = Math.ceil(rect.width / w);
-                debugLog("Other win: x,y w,h=" + rect.x + "," + rect.y + " " + rect.width + "," + rect.height + " (" + windows[i].get_title() + ")");
-                debugLog("r,c rows,cols=" + row0 + "," + col0 + " " + rowCnt + "," + colCnt);
-                for (let r = row0; r < Math.min(row0 + rowCnt, rows); r++) {
-                    for (let c = col0; c < Math.min(col0 + colCnt, cols); c++) {
-                        debugLog("Occupied row=" + r + ", col=" + c + " (" + windows[i].get_title() + ")");
-                        squares[r][c].push(windows[i]);
-                    }
-                }
-            }
-        }
-
-        debugLog("Occupied pos rows=" + rows + ", cols=" + cols);
-        for (let r = 0; r < rows; r++) {
-            let row = "";
-            for (let c = 0; c < cols; c++) {
-                if (squares[r][c].length > 0) {
-                    row = row + "o";
-                } else {
-                    row = row + " ";
-                }
-            }
-            debugLog("(" + r + ") : " + row);
-        }
-
-        // Find first available slot
-        let foundPlacement = false;
-        let foundRow;
-        let foundCol;
-        for (let c = 0; c < cols && !foundPlacement; c++) {
-            for (let r = 0; r < rows; r++) {
-                if (squares[r][c].length === 0) {
-                    foundRow = r;
-                    foundCol = c;
-                    x = c * w;
-                    y = r * h;
-                    debugLog("Found slot at row=" + r + ", col=" + c + " -> " + x + ", " + y + " (" + win.get_title() + ")");
-                    foundPlacement = true;
-                    break;
-                }
-            }
-        }
-
-        if (foundPlacement) {
-            debugLog("window (" + win.get_title() + ") rough placement is " + x + ", " + y);
-            // Pack placement north
-            let packedN = false;
-            if (foundRow > 0) {
-                const r = foundRow - 1;
-                const windowsN = squares[r][foundCol];
-                y = windowsN.map((win) => win.get_frame_rect()).reduce(
-                    (acc, rect) => Math.max(acc, rect.y + rect.height),
-                    h * r
-                );
-                packedN = true;
-                debugLog("window (" + win.get_title() + ") moved north to y=" + y);
-            }
-
-            // Pack placement left
-            if (foundCol > 0) {
-                const c = foundCol - 1;
-                const windowsW = squares[foundRow][c];
-                const maxXW = windowsW.map((win) => win.get_frame_rect()).reduce(
-                    (acc, rect) => Math.max(acc, rect.x + rect.width),
-                    w * c
-                );
-                let maxXNW = maxXW;
-                if (packedN) {
-                    const windowsNW = squares[foundRow - 1][c];
-                    maxXNW = windowsNW.map((win) => win.get_frame_rect()).reduce(
-                        (acc, rect) => Math.max(acc, rect.x + rect.width),
-                        w * c
-                    );
-                }
-                x = Math.max(maxXNW, maxXW);
-                debugLog("window (" + win.get_title() + ") moved west to x=" + x);
-            }
-
-            if (x === 0) {
-                x = x + minX;
-            }
-            if (y === 0) {
-                y = y + minY;
-            }
-
-            if (win.decorated) {
-                win.move_frame(true, x, y);
-            } else {
-                win.move(true, x, y);
-            }
-            debugLog("window (" + win.get_title() + ") placed at " + x + ", " + y);
+        if (win.decorated) {
+            win.move_frame(true, x, y);
         } else {
-            debugLog("Found no placement for (" + win.get_title() + ")");
+            win.move(true, x, y);
         }
+        debugLog("Window \"" + win.get_title() + "\" placed at " + x + ", " + y);
     }
 }
 
@@ -181,7 +69,10 @@ function _onWorkspaceAdded() {
     let workspace;
     for (let i = 0; i < global.screen.n_workspaces; i++) {
         workspace = global.screen.get_workspace_by_index(i);
-        this._signals.push(workspace.connect('window-added', Lang.bind(this, this._onWindowAdded)));
+        this._signals.push(workspace.connect(
+          'window-added',
+          Lang.bind(this, this._onWindowAdded)
+        ));
     }
 }
 
@@ -198,13 +89,13 @@ function init() {
 }
 
 function enable() {
-    infoLog("enable");
+    debugLog("enable");
     this.workspaceAddedId = global.screen.connect('workspace-added', Lang.bind(this, this._onWorkspaceAdded));
     this._onWorkspaceAdded();
 };
 
 function disable() {
-    infoLog("disable");
+    debugLog("disable");
     global.screen.disconnect(workspaceAddedId);
     this._onDisconnectSignals();
 };
